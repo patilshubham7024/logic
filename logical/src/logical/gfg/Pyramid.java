a954a7a8b1ad0d33413dc9c47ee8be7462f5bdc4
Shubham Patil <patilshubham7024@gmail.com>
2025-11-11 00:38:00 +0530

    Pyramid logic - java 11

--- logical/src/logical/gfg/Pyramid.java
+++ logical/src/logical/gfg/Pyramid.java
@@ -14,13 +14,15 @@ public class Pyramid {
 	public static void draw(int input) {
 
 		for (int i = 1; i <= input; i++) {
-			for (int j = 1; j <= input; j++) {
-				if(j <= input - i) {
-					System.out.print(" ");
-				} else if (j <= input) {
-					System.out.print("* ");
-				}
-			}
+			System.out.print(" ".repeat(input - i));
+			System.out.print("* ".repeat(i));
+//			for (int j = 1; j <= input; j++) {
+//				if (j <= input - i) {
+//					System.out.print(" ");
+//				} else if (j <= input) {
+//					System.out.print("* ");
+//				}
+//			}
 			System.out.println();
 		}
 	}
